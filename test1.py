b0c82d895758665141f6b7e76901680be987636f
Vinícius Martins <vinicius270502@gmail.com>
2024-02-22 15:56:37 -0300

    Creation of Repository

---
+++ test1.py
@@ -0,0 +1,33 @@
+import mediapipe as mp 
+import cv2
+import numpy as np
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+cap = cv2.VideoCapture(1)
+
+# Initiate Holistic Model
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        # Recolor Image
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+
+        # Make Detections
+        results = pose.process(image)
+
+        image.flags.writeable = True
+        image = cv2.cvtColor(frame, cv2.COLOR_RBG2BGR)
+
+
+        cv2.imshow('Pose Model FRAME Detector', frame)
+        
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
