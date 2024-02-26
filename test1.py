c333cb8b10ff6b26c4a64f192e88dce254549a22
Vinícius Martins <vinicius270502@gmail.com>
2024-02-26 15:30:02 -0300

    Just some regular tests, nothing wow

--- test1.py
+++ test1.py
@@ -2,36 +2,19 @@
 ##### THIS IS THE BEST PROGRAMMER'S BRANCH, QUIT IT BITCH ######
 ################################################################
 
-import mediapipe as mp 
 import cv2
+import mediapipe as mp
 import numpy as np
-
 mp_drawing = mp.solutions.drawing_utils
-mp_pose = mp.solutions.pose
+mp.pose = mp.solutions.pose
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    cv2.imshow('Mediapipe Feed', frame)
 
-# Initiate Holistic Model
-with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-
-        # Recolor Image
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
-
-        # Make Detections
-        results = pose.process(image)
-
-        image.flags.writeable = True
-        image = cv2.cvtColor(frame, cv2.COLOR_RBG2BGR)
-
-
-        cv2.imshow('Pose Model FRAME Detector', frame)
-        
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
+    if cv2.waitKey(10) & 0xFF == ord ('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
