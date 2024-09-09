89d68317cb403445dfc011d57b4f83dc2482478a
Pedro3211 <peeeedro3211@gmail.com>
2024-09-09 19:59:42 -0300

    Criação da interface

--- test1.py
+++ test1.py
@@ -1,70 +1,71 @@
-from keras.models import load_model  # TensorFlow is required for Keras to work
 import cv2
+from typing import NoReturn
+import tkinter as tk
+import customtkinter
 import mediapipe as mp
 import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-# Disable scientific notation for clarity
-np.set_printoptions(suppress=True)
-
-# Load the model
-model = load_model("C:/MaterialTreinoIA/ModeloTreinado/keras_Model.h5", compile=False)
-
-# Load the labels
-class_names = open("C:/MaterialTreinoIA/ModeloTreinado/labels.txt", "r").readlines()
-
-# CAMERA can be 0 or 1 based on default camera of your computer
-cap = cv2.VideoCapture(0)
-## Setup mediapipe instance
-with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
-        ret, frame = cap.read()
-        
-        # Recolor image to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
-      
-        # Make detection
-        results = pose.process(image)
     
-        # Recolor back to BGR
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        
-        image2 = cv2.resize(frame, (224, 224), interpolation=cv2.INTER_AREA)
+def main() -> NoReturn:
+    
+    def AbrirCamera():
+        cap = cv2.VideoCapture(int(camera.get()))
+        ## Setup mediapipe instance
+        with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+            while cap.isOpened():
+                ret, frame = cap.read()
+                
+                # Recolor image to RGB
+                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                image.flags.writeable = False
+            
+                # Make detection
+                results = pose.process(image)
+            
+                # Recolor back to BGR
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                
+                # Render detections
+                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                        mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                        mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                        )               
+                
+                cv2.imshow('Retorno camera '+camera.get(), image)
 
-        # Make the image a numpy array and reshape it to the models input shape.
-        image2 = np.asarray(image2, dtype=np.float32).reshape(1, 224, 224, 3)
+                #
 
-        # Normalize the image array
-        image2 = (image2 / 127.5) - 1
-        
-        prediction = model.predict(image2)
-        index = np.argmax(prediction)
-        class_name = class_names[index]
-        confidence_score = prediction[0][index]
-        
-        # Render detections
-        if index == 0:       
-            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                    mp_drawing.DrawingSpec(color=(0,0,0), thickness=2, circle_radius=2), 
-                                    mp_drawing.DrawingSpec(color=(0,255,0), thickness=2, circle_radius=2) 
-                                    )  
-        else:
-            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                    mp_drawing.DrawingSpec(color=(0,0,0), thickness=2, circle_radius=2), 
-                                    mp_drawing.DrawingSpec(color=(0,0,255), thickness=2, circle_radius=2) 
-                                    )                  
+            cap.release()
+            cv2.destroyAllWindows()
+            
+    
+    customtkinter.set_appearance_mode("System")
+    customtkinter.set_default_color_theme("blue")
 
-        # Print prediction and confidence score
-        print("Class:", class_name[2:], end="")
-        print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
-        
-        cv2.imshow('Mediapipe Feed', image)
-
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+    app = customtkinter.CTk()
+    
+    app = customtkinter.CTk()
+    app.geometry("480x250")
+    app.title("Reconhecimento da câmera")
+    
+    lbl_camera = customtkinter.CTkLabel(app, text="Escolha um dispositivo de captura:", font=customtkinter.CTkFont(size=15, weight="bold"))
+    lbl_camera.place(x=125, y=30)
+    
+    camera = customtkinter.CTkEntry(app, placeholder_text="0, 1, 2...", width=100)
+    camera.pack(pady=75, padx=5)                 
+    
+    ButtonAbrir = customtkinter.CTkButton(app, text="Abrir Camera", command=AbrirCamera)
+    ButtonAbrir.pack(pady=10, padx=10)
+    
+    # ButtonFechar = customtkinter.CTkButton(app, text="Fechar Camera")
+    # ButtonFechar.pack(pady=10, padx=10)
+    
+    app.mainloop()
+    
+if __name__ == '__main__':
+    main()
+    
+    
